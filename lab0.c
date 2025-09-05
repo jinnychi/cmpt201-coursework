db7ff7bdc07af9b6db09e140f579dbb629d718cd
Jin-Ya Lau <jinyalau@gmail.com>
2025-09-05 11:45:34 -0700

    test :)

--- lab0.c
+++ lab0.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-  printf("Hello CMPT201 World!\n");
-  printf("This is gonna be a wild ride!\n");
+  printf("Hello CMPT201 World! \n");
+  printf("This is gonna be a wild ride! \n");
 }
